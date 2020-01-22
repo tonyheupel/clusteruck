250dd6fe72ba19e47da752ecea7196a92f43b838
Tony Heupel <tony.heupel@alaskaair.com>
2020-01-22 10:14:50 -0800

    simplify code to only sleep 250 use case

--- dotnetcore/Controllers/HomeController.cs
+++ dotnetcore/Controllers/HomeController.cs
@@ -24,11 +24,5 @@ namespace clusteruck.Controllers
             await Task.Delay(TimeSpan.FromMilliseconds(250));
             return "hello, world\n";
         }
-
-//        [HttpGet]
-//        public string Get()
-//        {
-//            return "hello, world\n";
-//        }
     }
 }
--- go/clusteruck.go
+++ go/clusteruck.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"runtime"
-	"strconv"
 	"time"
 )
 
@@ -14,31 +12,8 @@ func slowHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "hello, world!\n")
 }
 
-func fastHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "hello, world!\n")
-}
-
-func resizeHandler(w http.ResponseWriter, req *http.Request) {
-	procs, err := strconv.Atoi(req.FormValue("workers"))
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Resize did not work.  Please use '?workers=<number>'")
-		return
-	}
-
-	fmt.Printf("sizing to %d procs...\n", procs)
-	fmt.Println("previous number of procs used:", runtime.GOMAXPROCS(procs))
-	fmt.Fprintf(w, "Resized to %d procs", procs)
-}
-
 func main() {
-	numCPU := runtime.NumCPU()
-	fmt.Println("number of procs:", numCPU)
-	fmt.Println(runtime.GOMAXPROCS(numCPU))
-
 	http.HandleFunc("/", slowHandler)
-	http.HandleFunc("/resize", resizeHandler)
 
 	http.ListenAndServe(":8081", nil)
 }
