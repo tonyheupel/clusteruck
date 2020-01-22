9e3e1118d3460e7891ce46bd33a06b3d8b3396f1
Tony Heupel <tony.heupel@alaskaair.com>
2020-01-22 15:37:20 -0800

    add runs for half a cpu

--- dotnetcore/run.sh
+++ dotnetcore/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# docker run --name clusteruck -p 8081:8081 --cpus=".5" --memory="100m" -d tchype/dotnetcore-clusteruck
 # docker run --name clusteruck -p 8081:8081 --cpuset-cpus="1" --memory="100m" -d tchype/dotnetcore-clusteruck
 docker run --name clusteruck -p 8081:8081 -d tchype/dotnetcore-clusteruck
--- go/run.sh
+++ go/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# docker run --name clusteruck -p 8081:8081 --cpus=".5" --memory="100m" -d tchype/go-clusteruck
 # docker run --name clusteruck -p 8081:8081 --cpuset-cpus="1" --memory="100m" -d tchype/go-clusteruck
 docker run --name clusteruck -p 8081:8081 -d tchype/go-clusteruck
