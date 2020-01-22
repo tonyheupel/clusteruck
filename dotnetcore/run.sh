00906be08c62eafffcb945eca3de954a1c2f5d3c
Tony Heupel <tony.heupel@alaskaair.com>
2020-01-22 11:25:10 -0800

    add back no restrictions during test

--- dotnetcore/run.sh
+++ dotnetcore/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run --name clusteruck -p 8081:8081 --cpuset-cpus="1" --memory="100m" -d tchype/dotnetcore-clusteruck
+# docker run --name clusteruck -p 8081:8081 --cpuset-cpus="1" --memory="100m" -d tchype/dotnetcore-clusteruck
+docker run --name clusteruck -p 8081:8081 -d tchype/dotnetcore-clusteruck
